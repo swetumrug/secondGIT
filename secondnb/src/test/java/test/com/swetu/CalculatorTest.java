454ae2abb219735347c7fd34ba07adcde05a3366
swetumrug <122067553+swetumrug@users.noreply.github.com>
2023-04-12 23:16:50 -0500

    initial files

---
+++ secondnb/src/test/java/test/com/swetu/CalculatorTest.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com.swetu;
+
+import com.mycompany.secondnb.Calculator;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author swetu
+ */
+public class CalculatorTest {
+    Calculator calc; // declare
+    
+    public CalculatorTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+        calc = new Calculator();//intiated
+        
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    
+    @Test
+    public void testAdd(){
+        assertEquals(calc.add(2, 4), 6);  
+    }
+    
+    @Test
+    public void testMul(){
+        assertEquals(calc.mul(2, 4), 8);
+    }
+}
